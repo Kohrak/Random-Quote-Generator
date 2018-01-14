bf51cc1c46941924aba7d10f2c5710ee7bb2c018
Kohrak <gabrielgt95@gmail.com>
2018-01-14 19:57:28 +0000

    fix request, display quote on quote element, autor name on name element, add getQuote function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 var wikiBtn = document.getElementById("btn");
 console.log("starting");
-wikiBtn.addEventListener("click", function(){
+function getQuote() {
+  console.log("getQuote")
   var wikiRequest = new XMLHttpRequest();
   wikiRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
   wikiRequest.onload = function(){
   var wikiData = JSON.parse(wikiRequest.responseText);
-  //document.getElementById("quote").innerHTML = "name: " +   wikiData[0].name;
-    console.log(wikiData[0].title + " " + wikiData[0]);
+  var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
+  document.getElementById("quote").innerHTML = quoteText;
+  document.getElementById("name").innerHTML = wikiData[0].title;
+    console.log(wikiData[0].title);
 };
-      wikiRequest.send(); 
-
-});
+      wikiRequest.send();
+}
+wikiBtn.addEventListener("click", getQuote());
