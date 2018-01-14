c53343560087a4c1bae2cd15d8c4a50987c06e9c
Kohrak <gabrielgt95@gmail.com>
2018-01-14 20:33:27 +0000

    move event to html, remove event listener, change request url filter

--- js/index.js
+++ js/index.js
@@ -3,14 +3,12 @@ console.log("starting");
 function getQuote() {
   console.log("getQuote")
   var wikiRequest = new XMLHttpRequest();
-  wikiRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
+  wikiRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand');
+  wikiRequest.send();
   wikiRequest.onload = function(){
   var wikiData = JSON.parse(wikiRequest.responseText);
   var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
   document.getElementById("quote").innerHTML = quoteText;
   document.getElementById("name").innerHTML = wikiData[0].title;
-    console.log(wikiData[0].title);
 };
-      wikiRequest.send();
 }
-wikiBtn.addEventListener("click", getQuote());
