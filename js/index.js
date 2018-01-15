61eb74537a7857033dcea72dae90b91046f1645e
Kohrak <gabrielgt95@gmail.com>
2018-01-15 01:33:26 +0000

    replace api, changed quote and title sizes

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@ var wikiBtn = document.getElementById("btn");
 console.log("starting");
 function getQuote() {
   console.log("getQuote")
+  var key = Math.floor((Math.random() * 999999));
   var wikiRequest = new XMLHttpRequest();
-  wikiRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand');
+  var url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=" + key;
+  wikiRequest.open('GET', url);
   wikiRequest.send();
   wikiRequest.onload = function(){
   var wikiData = JSON.parse(wikiRequest.responseText);
-  var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
-  document.getElementById("quote").innerHTML = quoteText;
-  document.getElementById("name").innerHTML = wikiData[0].title;
+  //var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
+  document.getElementById("quote").innerHTML = wikiData.quoteText;
+  document.getElementById("name").innerHTML = wikiData.quoteAuthor;
 };
 }
+getQuote();
