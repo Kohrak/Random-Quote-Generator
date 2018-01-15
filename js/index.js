da520752fa82e699b3b745488194db90a9022a19
Kohrak <gabrielgt95@gmail.com>
2018-01-15 13:41:17 +0000

    swap quote api for quotes on design api, add check for request status

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@ var wikiBtn = document.getElementById("btn");
 console.log("starting");
 function getQuote() {
   console.log("getQuote")
-  var key = Math.floor((Math.random() * 999999));
+  //var key = Math.floor((Math.random() * 999999));
   var wikiRequest = new XMLHttpRequest();
-  var url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=" + key;
-  wikiRequest.open('GET', url);
-  wikiRequest.send();
-  wikiRequest.onload = function(){
+  var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+  wikiRequest.onreadystatechange = function(){
+  if (this.readyState == 4 && this.status == 200) {
   var wikiData = JSON.parse(wikiRequest.responseText);
-  //var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
-  document.getElementById("quote").innerHTML = wikiData.quoteText;
-  document.getElementById("name").innerHTML = wikiData.quoteAuthor;
+  var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
+  document.getElementById("quote").innerHTML = wikiData[0].content;
+  document.getElementById("name").innerHTML = wikiData[0].title;
+}
 };
+  wikiRequest.open('GET', url, true);
+  wikiRequest.send();
 }
 getQuote();
