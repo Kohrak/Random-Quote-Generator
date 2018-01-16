da9a5589535da5e2d15e008483cad69f3807678f
Kohrak <gabrielgt95@gmail.com>
2018-01-16 21:36:46 +0000

    Add tweet functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var wikiBtn = document.getElementById("btn");
+var globalQuote = ""
 console.log("starting");
 function getQuote() {
   var key = Math.floor((Math.random() * 999999));
@@ -10,11 +11,20 @@ function getQuote() {
   var wikiData = JSON.parse(wikiRequest.responseText);
   //var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
   document.getElementById("quote").innerHTML = "<i class='fa fa-quote-left'></i>" + wikiData.quoteText + "<i class='fa fa-quote-right'></i>";
+
   if (wikiData.quoteAuthor == ""){
     document.getElementById("name").innerHTML = "-Anonymous";
   } else {
     document.getElementById("name").innerHTML = "-" + wikiData.quoteAuthor;
   }
+  if (wikiData != ""){
+    globalQuote = "\"" + wikiData.quoteText + "\" " + document.getElementById("name").innerHTML;
+  }
 };
+
 }
 getQuote();
+
+function tweet(tex) {
+  window.open("https://twitter.com/intent/tweet?text=" + tex);
+}
