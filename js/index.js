7241fda2eaaefc1b629bffc7e09c93627a5b020c
Kohrak <gabrielgt95@gmail.com>
2018-01-17 19:12:18 +0000

    Change the xhr request for a JSONP request, fix CORS problems

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 var wikiBtn = document.getElementById("btn");
 var globalQuote = ""
-console.log("starting");
-function getQuote() {
+function jsonpRequest(){
   var key = Math.floor((Math.random() * 999999));
-  var wikiRequest = new XMLHttpRequest();
-  var url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=" + key;
-  wikiRequest.open('GET', url);
-  wikiRequest.send();
-  wikiRequest.onload = function(){
-  var wikiData = JSON.parse(wikiRequest.responseText);
-  //var quoteText = wikiData[0].content.replace("<p>", "").replace("</p>", "")
+  var tag = document.createElement("script");
+  tag.src = "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=getQuote&key=" + key;
+  document.getElementsByTagName("head")[0].appendChild(tag);
+}
+function getQuote(wikiData) {
   document.getElementById("quote").innerHTML = "<i class='fa fa-quote-left'></i>" + wikiData.quoteText + "<i class='fa fa-quote-right'></i>";
-
   if (wikiData.quoteAuthor == ""){
     document.getElementById("name").innerHTML = "-Anonymous";
   } else {
@@ -22,9 +18,8 @@ function getQuote() {
   }
 };
 
+function tweet(text) {
+  window.open("https://twitter.com/intent/tweet?text=" + text);
 }
-getQuote();
 
-function tweet(tex) {
-  window.open("https://twitter.com/intent/tweet?text=" + tex);
-}
+jsonpRequest();
